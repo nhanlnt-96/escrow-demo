da347f8145c1e7edc55da8b8e53a7fee742875c0
Thien Nhan <nhanlnt@hotmail.com>
2022-06-05 21:16:43 +0700

    master: initial project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import MainLayout from './layouts/MainLayout';
+import Escrow from './pages/Escrow';
+
+import './assets/styles/styles.scss'
+
+function App() {
+  return (
+    <MainLayout>
+      <Escrow />
+    </MainLayout>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import { useEthers, shortenAddress } from '@usedapp/core'
+import React from 'react'
+
+export default function Header() {
+
+  const { account, chainId, deactivate, activateBrowserWallet } = useEthers()
+
+  const handleConnect = () => {
+    if(!account) {
+      activateBrowserWallet()
+    }
+    else {
+      deactivate()
+    }
+  }
+
+  return (
+    <div>
+      <header style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '4px 20px'
+      }}>
+        <h1>Escrow</h1>
+        <button onClick={handleConnect}>
+          { account ? shortenAddress(account) : 'Connect Wallet' }
+        </button>
+      </header>
+      {
+        chainId !== 80001 ?
+          <p style={{color: 'red'}}>Wrong network. Please change your network.</p>
+        : ''
+      }
+    </div>
+  )
+}
