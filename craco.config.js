fdf7e3843413a4869444f1b1932f06440a27bada
nhanlnt <nhanlnt@smartosc.com>
2022-07-18 13:04:19 +0700

    complete config craco-alias

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  plugins: [
+    {
+      plugin: require("craco-alias"),
+      options: {
+        baseUrl: "./src",
+        source: "jsconfig",
+      },
+    },
+  ],
   style: {
     postcss: {
       plugins: [require("tailwindcss"), require("autoprefixer")],
