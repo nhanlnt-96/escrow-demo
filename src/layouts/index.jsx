b42a3aded71f0968bc8be21a67ee880e01a1f523
nhanlnt-96 <nhanlnt@hotmail.com>
2022-07-19 01:40:58 +0700

    working on toast position

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,25 +1,25 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { routes } from "configs";
-import Header from "../components/Header";
+import {Route, Routes} from "react-router-dom";
+import {routes} from "configs";
 import HeaderComp from "../components/headerComp";
-import ChooseCurrencyPopup from "../components/chooseCurrencyPopup";
 
 const MainLayout = () => {
   return (
-    <div className="w-screen h-screen">
-      <HeaderComp />
-      <Routes>
-        {routes.map((val, index) => (
-          <Route
-            key={index}
-            path={val.path}
-            element={val.module}
-            exact={val.isExact}
-          />
-        ))}
-      </Routes>
-      <ChooseCurrencyPopup />
+    <div className="w-screen h-screen relative">
+      <HeaderComp/>
+      <div className="mt-16">
+        <Routes>
+          {routes.map((val, index) => (
+            <Route
+              key={index}
+              path={val.path}
+              element={val.module}
+              exact={val.isExact}
+            />
+          ))}
+        </Routes>
+      </div>
+      {/*<ChooseCurrencyPopup />*/}
     </div>
   );
 };
